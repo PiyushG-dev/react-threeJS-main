10e5dfed93bc037ede4b37c8d3481f668416b34d
Piyush Goswami <pjgpiyush@gmail.com>
2024-03-28 18:59:17 +0530

    hero ui completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { smallHeroVideo, heroVideo } from "../utils";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSrc = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrc);
+
+    return () => {
+      window.removeEventListener("resize", handleVideoSrc);
+    };
+  }, []);
+
+  return (
+    <section className="nav-height relative w-full">
+      <div className="flex items-center justify-center h-5/6">
+        <div className="md:w-10/12 w-10/12">
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} />
+          </video>
+        </div>
+      </div>
+      <div className="w-full flex justify-center flex-col items-center">
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p>From $199/month or $999</p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
